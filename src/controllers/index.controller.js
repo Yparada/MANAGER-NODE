780f53432054584bc53ad4aaad008955d7b0a325
Yparada <yparadat@gmail.com>
2022-02-15 22:36:55 -0500

    Find user by id added

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -13,14 +13,27 @@ const getUsers = async (req, res) => {
     res.status(200).json(response.rows);
 }
 
+const getUserById = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('select * from users where id = $1',[id]);
+    res.json(response.rows);
+}
+
 const createUser = async (req, res) => {
     const { name, email } = req.body;
     const response = await pool.query('insert into users (name, email) values ($1, $2)', [name, email]);
     console.log(response);
-    res.send('User create')
+    res.json({
+        message: 'User added succesfuly',
+        body: {
+            user: {name, email}
+        }
+
+    });
 }
 
 module.exports = {
     getUsers,
-    createUser
+    createUser,
+    getUserById
 }
